9a5038d416e7b1f68baa1f9293935c0effd361f2
Kwiri123 <86187274+Kwiri123@users.noreply.github.com>
2022-03-16 21:15:20 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -5,5 +5,7 @@ $mysqli = new mysqli("sql4.freemysqlhosting.net","sql4479459","l5taK4X6gf","sql4
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
+}else{
+  echo "Yes";
 }
 ?>
