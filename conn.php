f863c3ff2d924196a9d4e02513afb54581aa897b
Kwiri123 <86187274+Kwiri123@users.noreply.github.com>
2022-03-16 22:01:16 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,7 +3,7 @@ $mysqli = new mysqli("mysql-72352-0.cloudclusters.net","root","slava1231802","ni
 
 // Check connection
 if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  echo "xueta";
   exit();
 }else{
   echo "Yes";
